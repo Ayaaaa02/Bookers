1c7666e791ad1eb7b5ef2dca7a51a1157bccc873
Ayaaaa02 <139942884+Ayaaaa02@users.noreply.github.com>
2023-08-11 02:02:02 +0000

    課題作成中

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,42 @@
+class BooksController < ApplicationController
+ def create
+    #１.&2. データを受け取り新規登録するためのインスタンス作成
+   book = Book.new(book_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+   if @book.save
+   # 4. トップ画面へリダイレクト
+   redirect_to book_path(book.id)
+   else 
+       @books = Book.all
+   end
+ end
+  
+  def index
+      @books =Book.all
+      @book = Book.new
+  end
+
+  def show
+      @book = Book.find(params[:id])  
+  end
+
+  def edit
+      @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)  
+  end
+  
+  def destroy
+   @book = Book.find(params[:id])
+  end
+  
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
