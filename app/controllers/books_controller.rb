cc4c27edc475836f0f0531305573a5b978303ecd
Ayaaaa02 <139942884+Ayaaaa02@users.noreply.github.com>
2023-08-15 01:55:26 +0000

    課題2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,15 @@
 class BooksController < ApplicationController
  def create
     #１.&2. データを受け取り新規登録するためのインスタンス作成
-   book = Book.new(book_params)
+   @book = Book.new(book_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
    if @book.save
    # 4. トップ画面へリダイレクト
-   redirect_to book_path(book.id)
+    flash[:notice] = "Book was successfully created."
+   redirect_to book_path(@book.id)
    else 
        @books = Book.all
+       render 'index'
    end
  end
   
@@ -25,13 +27,19 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)  
+   @book = Book.find(params[:id])
+   if @book.update(book_params)
+    flash[:notice] = "Book was successfully updated"
+    redirect_to book_path(@book.id)  
+   else
+    render "edit"
+   end
   end
   
   def destroy
-   @book = Book.find(params[:id])
+  @book = Book.find(params[:id])
+  @book.destroy
+    redirect_to books_path
   end
   
   private
